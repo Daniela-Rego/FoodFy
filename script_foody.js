78079caa8f58c3074bf9e832243313e6b452294e
Daniela Rego <50534291+Daniela-Rego@users.noreply.github.com>
2020-07-04 12:13:13 -0300

    Primeiro modulo do projeto de conclusao

---
+++ script_foody.js
@@ -0,0 +1,26 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const comida = document.querySelectorAll('.alimento');
+
+for(let card of comida)
+{
+    card.addEventListener("click",function(){
+        const imgID = card.getAttribute("id");
+         //quando eu quiser pegar o conteudo da tag usa innerHTML
+        //const tegP = card.querySelector("#chefs");// "<p id="chefs"> por Jorge Relato</p> "aqui pega teg inteira
+        const tegP = card.querySelector("#chefs").innerHTML;//"por Jorge Relato" aqui pega somente o conteudo
+        console.log(tegP);
+       
+        modalOverlay.classList.add('active');
+       
+        modalOverlay.querySelector('.chef').innerHTML =tegP;        
+        modalOverlay.querySelector("img").src = `img/${imgID}` ;
+        //modalOverlay.querySelector("iframe").src = `img/${imgID}` ;
+    });
+}
+
+document.querySelector(".close-modal").addEventListener("click",function(){
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("img").src="";
+    
+    //modalOverlay.querySelectorAll("iframe").src="";
+});
